fed3dc7a6e7e7fec30b0055c3ce163801fcfbd30
Carlos Daniel <carlosdanielcarlinhos874@gmail.com>
2026-09-06 12:42:12 -0300

    Revert "Segunda alteração"
    This reverts commit cce1e0185d34dc204abc7c4f8372abe0b9b2e894.

--- src/aleatoria.java
+++ src/aleatoria.java
@@ -4,26 +4,14 @@ public class aleatoria {
 
     public static void main(String[] args) {
 
-        int age = Integer.parseInt(enter("Qual a sua idade?"));
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Qual sua idade?"));
 
         if (age >= 18) {
 
-            write("Você é maior de idade");
+            JOptionPane.showMessageDialog(null, "Você é maior de idade.");
         } else {
 
-            write("Você é menor de idade");
+            JOptionPane.showMessageDialog(null, "Você é menor de idade.");
         }
-    }
-
-    private static void write(String message) {
-
-        JOptionPane.showMessageDialog(null, message);
-
-    }
-
-    private static String enter(String message) {
-
-        return JOptionPane.showInputDialog(message);
-
     }
 }
