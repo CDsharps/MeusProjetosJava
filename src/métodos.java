c4bcb46ae967008aa531914d0ab48c865a982d81
Carlos Daniel <carlosdanielcarlinhos874@gmail.com>
2026-09-25 14:37:02 -0300

    Código número 2 concluído

--- src/métodos.java
+++ src/métodos.java
@@ -24,14 +24,14 @@ public class métodos {
             switch (option) {
 
                 case 1:
-                    int[][] timesA = constructMatrix("A");
-                    if(timesA == null){
+                    int[][] timesA = constructMatrix("A ");
+                    if (timesA == null) {
 
                         break;
                     }
 
-                    int[][] timesB = constructMatrix("B");
-                    if(timesB == null){
+                    int[][] timesB = constructMatrix("B ");
+                    if (timesB == null) {
 
                         break;
                     }
@@ -40,14 +40,26 @@ public class métodos {
                     break;
 
                 case 2:
-                    int[][] plusA = constructMatrix("A");
-                    if(plusA == null){
+                    int[][] plusA = constructMatrix("A ");
+                    if (plusA == null) {
 
                         break;
                     }
 
-                    int[][] plusB = constructMatrix("B");
-                    if(plusB == null){
+                    int[] plusBparts = errorProportion("Digite a proporção da matriz B (?x?): ");
+                    if (plusBparts == null) {
+
+                        break;
+                    }
+
+                    if (plusA.length != plusBparts[0] || plusA[0].length != plusBparts[1]) {
+
+                        write("Não é possível efetuar a soma, as matrizes devem ter a mesma proporção!");
+                        break;
+                    }
+
+                    int[][] plusB = new int[plusA.length][plusA[0].length];
+                    if (!fillMatrix(plusB, plusB.length, plusB[0].length, "Digite o número da posição ")) {
 
                         break;
                     }
@@ -55,17 +67,158 @@ public class métodos {
                     sumMatrix(plusA, plusB);
                     break;
 
+                case 3:
+                    int[] identParts = errorProportion("Digite a proporção da matriz (?x?): ");
+                    if (identParts == null) {
+
+                        break;
+                    }
+
+                    int lines = identParts[0];
+                    int columns = identParts[1];
+
+                    if (lines != columns) {
+
+                        write("Essa matriz NÃO é uma matriz identidade!");
+                        break;
+                    }
+
+                    int[][] ident = new int[lines][columns];
+                    if (!fillMatrix(ident, lines, columns, "Digite o número da posição ")) {
+
+                        break;
+                    }
+
+                    if (!identityMatrix(ident)) {
+
+                        write("Essa matriz NÃO é uma matriz identidade!");
+                        break;
+                    } else {
+
+                        write("Essa matriz É uma matriz identidade!");
+                        break;
+                    }
+
+                case 4:
+                    int[][] transpost = constructMatrix("");
+                    if (transpost == null) {
+
+                        break;
+                    }
+
+                    transpostMatrix(transpost);
+                    break;
+
+                case 5:
+                    int[][] print = constructMatrix("");
+                    if (print == null) {
+
+                        break;
+                    }
+
+                    write("MATRIZ RECEBIDA\n\n" + printMatrix(print));
+                    break;
+
                 case 6:
                     write("Obrigado pela preferência!");
                     break;
+
             }
         } while (option != 6);
     }
 
-    private static void sumMatrix(int[][] matrixA, int[][] matrixB) {
+    // MÉTODO DE IMPRIMIR MATRIZ
+    private static String printMatrix(int[][] matrix) {
+
+        StringBuilder text = new StringBuilder();
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+
+                text.append("[").append(matrix[i][j]).append("] ");
+            }
+
+            text.append("\n");
+        }
+
+        return text.toString();
 
     }
 
+    // MÉTODO DE MATRIZ TRANSPOSTA
+    private static void transpostMatrix(int[][] matrix) {
+
+        int linesT = matrix.length;
+        int columnsT = matrix[0].length;
+
+        int[][] transposed = new int[columnsT][linesT];
+
+        for (int j = 0; j < transposed.length; j++) {
+            for (int i = 0; i < transposed[0].length; i++) {
+
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+
+        write("MATRIZ TRANSPOSTA\n\n" + printMatrix(transposed));
+
+    }
+
+    // MÉTODO DE MATRIZ IDENTIDADE
+    private static boolean identityMatrix(int[][] identity) {
+
+        if (identity.length != identity[0].length) {
+
+            return false;
+        }
+
+        int verify = 0;
+
+        for (int i = 0; i < identity.length; i++) {
+            for (int j = 0; j < identity[0].length; j++) {
+
+                if (i == j) {
+                    if (identity[i][j] != 1) {
+
+                        verify++;
+                    }
+                }
+
+                if (i != j) {
+                    if (identity[i][j] != 0) {
+
+                        verify++;
+                    }
+                }
+            }
+        }
+
+        if (verify != 0) {
+
+            return false;
+        }
+
+        return true;
+
+    }
+
+    // MÉTODO DE SOMA DE MATRIZES
+    private static void sumMatrix(int[][] matrixA, int[][] matrixB) {
+
+        int[][] result = new int[matrixA.length][matrixB[0].length];
+
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+
+                result[i][j] = matrixA[i][j] + matrixB[i][j];
+            }
+        }
+
+        write("SOMA DAS MATRIZES 'A' E 'B'\n\n" + printMatrix(result));
+
+    }
+
+    // MÉTODO DE MULTIPLICAÇÃO DE MATRIZES
     private static void multiplyMatrix(int[][] matrixA, int[][] matrixB) {
 
         int linesA = matrixA.length;
@@ -95,29 +248,13 @@ public class métodos {
             }
         }
 
-        write("MATRIZ C\n\n" + matrixToAnswer(result));
+        write("MULTIPLICAÇÃO DAS MATRIZES 'A' E 'B'\n\n" + printMatrix(result));
     }
 
-    private static String matrixToAnswer(int[][] matrix) {
-
-        StringBuilder text = new StringBuilder();
-
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-
-                text.append("[").append(matrix[i][j]).append("]");
-            }
-
-            text.append("\n");
-        }
-
-        return text.toString();
-
-    }
-
+    // CONSTRUÇÃO DE MATRIZ (OTIMIZAÇÃO AUTORAL)
     private static int[][] constructMatrix(String letter) {
 
-        int[] parts = errorProportion("Digite a proporção da matriz " + letter + " (?x?): ");
+        int[] parts = errorProportion("Digite a proporção da matriz " + letter + "(?x?): ");
         if (parts == null) {
 
             return null;
@@ -132,9 +269,40 @@ public class métodos {
             return null;
         }
 
-		return matrix;
+        return matrix;
     }
 
+    // PREENCHIMENTO DE MATRIZES (OTIMIZAÇÃO AUTORAL)
+    private static boolean fillMatrix(int[][] matrix, int lines, int columns, String message) {
+
+        for (int i = 0; i < lines; i++) {
+            for (int j = 0; j < columns; j++) {
+
+                matrix[i][j] = errorInt(message + "[" + (i + 1) + "][" + (j + 1) + "]:");
+                if (matrix[i][j] == Integer.MIN_VALUE) {
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
+
+    }
+
+    private static void write(String args) {
+
+        JOptionPane.showMessageDialog(null, args);
+
+    }
+
+    private static String enter(String args) {
+
+        return JOptionPane.showInputDialog(args);
+
+    }
+
+    // TRATAMENTOS DE ERROS (OTIMIZAÇÃO AUTORAL)
     private static int[] errorProportion(String message) {
 
         while (true) {
@@ -168,35 +336,6 @@ public class métodos {
                 write("Formato Inválido. Digite no formato ?x?");
             }
         }
-    }
-
-    private static boolean fillMatrix(int[][] matrix, int lines, int columns, String message) {
-
-        for (int i = 0; i < lines; i++) {
-            for (int j = 0; j < columns; j++) {
-
-                matrix[i][j] = errorInt(message + "[" + (i + 1) + "][" + (j + 1) + "]:");
-                if (matrix[i][j] == Integer.MIN_VALUE) {
-
-                    return false;
-                }
-            }
-        }
-
-        return true;
-
-    }
-
-    private static void write(String args) {
-
-        JOptionPane.showMessageDialog(null, args);
-
-    }
-
-    private static String enter(String args) {
-
-        return JOptionPane.showInputDialog(args);
-
     }
 
     private static String errorString(String message) {
